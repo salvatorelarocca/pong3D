f7df369e2cb4e315ec58cd5351dbc388f1198e03
heartbeatLV <lscorionjs@gmail.com>
2022-10-28 15:13:58 +0200

    add install lib task

---
+++ install-libs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin
+
+sudo apt-get install libglew-dev
+sudo apt-get install libsoil-dev
+sudo apt-get install libglu1-mesa-dev
+sudo apt-get install freeglut3-dev
+sudo apt-get install mesa-common-dev
